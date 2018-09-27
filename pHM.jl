b95e0defc79711cfe956c6187e934b6f6f541a20
Tanner Lee <tannerlee@mymail.mines.edu>
2018-09-27 17:14:47 -0600

    Added logic to print a grid to png.  Next step is to read a grid from a .csv file and print it to grid.

--- pHM.jl
+++ pHM.jl
@@ -6,14 +6,31 @@
 #############################
 
 # Initialize Luxor
+println("!Initializing Luxor Package!")
 using Luxor
-Drawing(500, 500, "images/pHM.png")
+println("!Luxor Initialized!")
+Drawing(1250, 1250, "images/pHM.png")
 origin()
 background("white")
 
-randomhue()
-text("HELLOWORLD!")
+# Initialize Table and digits
+table = Table(25, 25, 50, 50) # 10 rows, 10 columns, 50 wide, 35 high
+digits = 1:625
 
-println("Printing to file!")
+# Print black text in the grid
+sethue("black")
+for n in 1:length(table)
+   text(string(digits[n]), table[n], halign=:center, valign=:middle)
+end
+
+# Print black borders on each of the grid boxes
+setopacity(0.5)
+sethue("black")
+for index in table
+    box.(index[1], 50, 50, :stroke) # row 3, every column
+end
+
+# Finish the image
+println("!Printing to file!")
 finish()
-println("Completed!")
+println("!Completed!")
