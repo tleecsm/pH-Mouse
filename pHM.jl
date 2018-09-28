62c44fe907e52bf4a49cf24a8ce06752471e723b
Tanner Lee <tannerlee@mymail.mines.edu>
2018-09-28 15:43:04 -0600

    Added a phm file that contains the data for the maze.  Also added initial logic to parse this input file.

--- pHM.jl
+++ pHM.jl
@@ -5,15 +5,72 @@
 #      @date 9/27/2018      #
 #############################
 
-# Initialize Room Size
+# Start by loading the initializing data in memory
+using DelimitedFiles
+println("!IMPORTING DATA FROM input.phm!")
+A = readdlm("input.phm", String)
+println("!DATA IMPORTED SUCCESSFULLY!")
+
+Arows = size(A)[1]
+Acols = size(A)[2]
+
+# Create an empty matrix we can concat with
+# The columns we want are the pivot columns
+probMatrix = zeros(Arows*Acols,Arows*Acols)
+
+# Parse through A to create probability matrix
+for i=1:Arows       # 1 < i < rows
+    for j=1:Acols   # 1 < j < cols
+        global A
+        global B
+        tempString = ""
+        tempRoomNumber = 0
+        # Check how many neighbors A[i,j] has
+        # A[i,j] has neighbors at --
+        # A[i+1,j], A[i-1,j], A[i,j+1], A[i,j-1]
+        # However all of these may not exist
+        # Boundary conditions must be considered
+        if i != 1
+            # We are not on the top row, decrease row
+            tempString = A[i-1,j]
+            # Get the room number from this neighbor
+            tempRoomNumber = chop(tempString)
+        end
+        if i != Arows
+            # We are not on the last row, increase row
+            tempString = A[i+1,j]
+            # Get the room number from this neighbor
+            tempRoomNumber = chop(tempString)
+        end
+        if j != 1
+            # We are not on the first col, decrease col
+            tempString = A[i,j-1]
+            # Get the room number from this neighbor
+            tempRoomNumber = chop(tempString)
+        end
+        if j != Acols
+            # We are not on the top row, decrease row
+            tempString = A[i,j+1]
+            # Get the room number from this neighbor
+            tempRoomNumber = chop(tempString)
+        end
+    end
+end
+
+
+
+
+
+#=
+# Initialize Room Size in pixels
 rSize = 100
 # Initialize Canvas Size
 cSize = rSize*5
 
 # Initialize Luxor
-println("!Initializing Luxor Package!")
+println("!INITIALIZING LUXOR PACKAGE!")
 using Luxor
-println("!Luxor Initialized!")
+println("!LUXOR INITIALIZED!")
 Drawing(cSize, cSize, "images/pHM.png")
 origin()
 background("white")
@@ -36,6 +93,7 @@ for index in table
 end
 
 # Finish the image
-println("!Printing to file!")
+println("!PRINTING OUTPUT IMAGE!")
 finish()
-println("!Completed!")
+println("!PRINTING COMPLETED!")
+=#
